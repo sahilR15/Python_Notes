737a3566458d954b285ba281bc2815f8bc227f4e
sahilR15 <sahilrawat1594@gmail.com>
2024-06-22 18:19:49 +0530

    first Three operators in python

---
+++ Operators.py
@@ -0,0 +1,157 @@
+# Operators
+
+# There are seven types of operators
+
+# 1. Airthmatic Operators
+# 2. Assigment Operators
+# 3. Comparision Operators
+# 4. Logical Operators
+# 5. Identity Operators
+# 6. Membership Operators
+# 7. Bitwise Operators
+
+
+#Airthmatic Operators
+
+# (+) : Addition
+# (-) : Substraction
+# (*) : Multiplication
+# (/) : Division
+# (%) : Modules  :- It shows the reminder of Two values
+# (**) :Exponentation :- It is used to add the power of first number.
+# (//) : Floor Division :- It show the value before the Decimal.
+
+# x = 45
+# y = 35
+
+# print("Addition :-",x+y)
+# print("Substraction :-",x-y)
+# print("Multiplication :-",x*y)
+# print("Division :-",x/y)
+
+# Modules :- It show the reminder.
+# x = 45
+# y = 20
+
+# print(x%y)
+
+# Floor Division :- It shows the value before the decimal.
+
+# x = 12
+# y = 5
+
+# print("Floor Division :-",x//y)
+
+# Exponentation :- 
+
+# x = 10
+# print(x**2)
+
+
+# x = 45
+# y = 63
+# z = 10
+
+# 1. Find the area of circle whose radius is 14.
+
+# pie = 22/7
+# r = 14
+# area = pie*r**2
+# print("Area of circle :-",area)
+
+# 2.  show the value of this simplification.
+# 3. x + y * z
+
+#a = x+y
+# print(a**2)
+# print(x+y+z)
+
+# 4. (x + y)**2
+
+# print(x+y,**2)
+
+# 5. Show the sum of these three number.
+
+#print(x+y+z)
+
+#==============================================================
+#==============================================================
+
+# 2. Assigment Operators 
+
+# =  : Assign 
+# += : Addition Equal   
+# -= : Substract Equal  
+# *= : Multiple Equal  
+# /+ : Division Equal   
+# // : Floor Equal
+# ** : Exponent Equal
+
+#========================================================
+#========================================================
+
+# Conditional Formating :- It is use to divide the data according 
+#                         to condition and print the statement if
+#                         condition is True or False.
+
+# 1. Print Yes if number is greater than 5 else Print No.
+
+# x = 20
+# if x>5:
+# 	print("Yes")
+# else:
+# 	print("No")
+
+# 2. Write a python program to check the number who is elegible
+#    for vote or not.
+
+# age = 17
+# if age>18:
+# 	print("Elegible")
+# else:
+# 	print("Not")
+
+# 3. write a pythone program to check the number is odd or even
+#    x = 56.
+
+# x = 15
+# y = x%2
+# if y==0:
+# 	print("Odd Number")
+# else:
+# 	print("Even Number")
+
+# 4. wirte a python programe to check the number divisible by
+#    5 or not. 
+
+# a = 15
+# if a%5==0:
+# 	print("Yes")
+# else:
+# 	print("Not")
+
+#==========================================================
+#==========================================================
+ 
+# Comparision Operators
+
+# 1. (>)  : Greater than
+# 2. (<)  : Less than 
+# 3. (>=) : Greater than equal to
+# 4. (<=) : Less than Equal to
+# 5. (==) : Equal to
+# 6. (!=) : Not equal to
+
+# User input :- It is used to enter the value in output section.
+
+# Number
+
+# x = int(input("Enter any Number :-"))
+# print(x*10)	 
+
+# x = int(input("Enter the age of Candidate :-"))
+
+# if x>=18:
+# 	print("Elegible for vote")
+# else:
+# 	print("Not Elegible for vote")
